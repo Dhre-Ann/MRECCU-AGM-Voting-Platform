56bb8f55bdbb567445b9dfa74df94d06ca37e117
Dhre_Ann <kd.dornelly@gmail.com>
2025-06-12 08:57:52 -0500

    webpack update, fixing bugs

--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,25 @@ const path = require('path');
 const webpack = require('webpack');
 const dotenv = require('dotenv');
 
-// Load environment variables from .env
-const env = dotenv.config().parsed || {};
+module.exports = () => {
+  // Load env variables
+  const env = dotenv.config().parsed || {};
 
-// Convert to a form DefinePlugin expects
-const envKeys = Object.keys(env).reduce((acc, key) => {
-  acc[`process.env.${key}`] = JSON.stringify(env[key]);
-  return acc;
-}, {});
+  // Convert for DefinePlugin
+  const envKeys = Object.keys(env).reduce((acc, key) => {
+    acc[`process.env.${key}`] = JSON.stringify(env[key]);
+    return acc;
+  }, {});
 
-module.exports = {
-  entry: ['./frontend/src/js/index.js'],
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'frontend/dist'),
-  },
-  mode: 'development', // Or 'production'
-  plugins: [
-    new webpack.DefinePlugin(envKeys)
-  ],
+  return {
+    entry: ['./frontend/src/js/index.js'],
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'frontend/dist'),
+    },
+    mode: 'development', // or 'production'
+    plugins: [
+      new webpack.DefinePlugin(envKeys)
+    ],
+  };
 };
